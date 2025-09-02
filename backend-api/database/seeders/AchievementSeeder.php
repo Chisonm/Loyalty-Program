a13dc408d620be93665fac13238356f54ea82602
chisonm <danielchisonum@gmail.com>
2025-09-02 11:32:36 +0300

    chore: Add achievement seeder and test data

---
+++ backend-api/database/seeders/AchievementSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Achievement;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AchievementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $achievements = [
+            [
+                'key' => 'first_purchase',
+                'name' => 'First Purchase',
+                'description' => 'Complete your very first purchase',
+                'icon' => 'Bag',
+                'requirement_type' => 'purchases_count',
+                'requirement_value' => 1,
+            ],
+            [
+                'key' => 'big_spender',
+                'name' => 'Big Spender',
+                'description' => 'Spend ₦10,000 in total',
+                'icon' => 'DollarSign',
+                'requirement_type' => 'total_spent',
+                'requirement_value' => 10000,
+            ],
+            [
+                'key' => 'loyal_customer',
+                'name' => 'Loyal Customer',
+                'description' => 'Make 5 purchases',
+                'icon' => 'Star',
+                'requirement_type' => 'purchases_count',
+                'requirement_value' => 5,
+            ],
+            [
+                'key' => 'premium_shopper',
+                'name' => 'Premium Shopper',
+                'description' => 'Spend ₦50,000 in total',
+                'icon' => 'Crown',
+                'requirement_type' => 'total_spent',
+                'requirement_value' => 50000,
+            ],
+        ];
+
+        foreach ($achievements as $achievement) {
+            Achievement::updateOrCreate(
+                ['key' => $achievement['key']],
+                $achievement
+            );
+        }
+    }
+}
